9dabd5dbaf9fbcd3fba6cd3746123482472f8dbb
Lloyd Miller <lloydinator@Lloyds-MacBook-Air.local>
2021-11-07 15:40:26 -0500

    First commit

--- web/pages/privacy.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="max-w-xl md:m-4 px-2 py-6 bg-white rounded shadow-xl h-screen">
-        <div class="p-4">
-            <p class="text-lg leading-loose">Credit cards are not saved but please don't add your personal credit card. It won't work but please don't try. Use 4242424242424242 to test the system out. I can't promise any email, phone number, address, etc. will not leak due to a breach. So please use dummy data as much as possible. However, the system sends out emails and texts so keep that in mind. Above all, just remember this is just a side-project and I'm just one person :)</p>
-        </div>
-    </div>
-</template>
